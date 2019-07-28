7db7d59d9d05894b1063406712ca4fca21c2af6f
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-28 13:35:22 -0700

    Updates

---
+++ precache-manifest.0041f6b8ce0a7ca2490f237fabcfd180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92d0e26d303cf0477d183a5df25681a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ff1d8dad2df5bde1b27f",
+    "url": "/clicky-game/static/css/main.d6df6b72.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "ff1d8dad2df5bde1b27f",
+    "url": "/clicky-game/static/js/main.b4a001bc.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
