090625a15fb0f542673487d047f98303f76f82cf
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-28 13:27:52 -0700

    Created data.js file which holds array of objects containing player name and image url to be dynamically rendered in App.js

---
+++ clicky-game/src/data.js
@@ -0,0 +1,28 @@
+var data = [
+    {
+        name: "Steph",
+        url: "https://cdn.vox-cdn.com/thumbor/1thMmPszN41RyZsjsoHTXwJEczM=/0x0:3552x5057/1200x800/filters:focal(1507x1263:2075x1831)/cdn.vox-cdn.com/uploads/chorus_image/image/63411102/1142665343.jpg.0.jpg"
+    },
+    {
+        name: "Draymond",
+        url: "https://thespun.com/wp-content/uploads/2019/04/GettyImages-1083447708-775x465.jpg"
+    },
+    {
+        name: "Klay",
+        url: "https://www.mercurynews.com/wp-content/uploads/2019/02/BNG-L-WARRIORS-0112-11-e1549016436322.jpg?w=620"
+    }, 
+    {
+        name: "Loon",
+        url: "https://www.foxsportsasia.com/uploads/2019/07/looney-kevon.jpg"
+    }, 
+    {
+        name: "Jacob",
+        url: "https://usatwarriorswire.files.wordpress.com/2018/07/gettyimages-993529780.jpg?w=1000&h=600&crop=1?w=1000&crop=0"
+    },
+    {
+        name: "Alfonzo",
+        url: "https://www.sfexaminer.com/wp-content/uploads/2019/04/16541129_web1_Alfonzo-McKinnie_1.jpg"
+    }
+];
+
+module.exports = data;
